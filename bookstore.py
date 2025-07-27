d67623a9b0b264171f249bc54111f85b5fa33101
Ram Saran <183719077+divyaramsaran@users.noreply.github.com>
2025-07-27 15:08:56 +0000

    adds removeInventory functionality

--- bookstore.py
+++ bookstore.py
@@ -3,13 +3,35 @@ def addInventory (inventory) :
     inventory.append(book)
     return inventory
 
+def removeInventory (inventory) :
+    inventoryToRemove = input('Enter A Book To Remove')
+    if (inventoryToRemove in inventory) :
+        inventory.remove(inventoryToRemove)
+        return inventory
+    print('The Book You Entered Is Not Present In Inventory')
+    return None
+
 
 def main() :
     inventory = ['Harry Potter', 'The Alchemist', 'Atomic Habits', 'Clean Code', 'Python Crash Course']
     harryWishList = {'Harry Potter', 'Deep Work', 'Clean Code'}
     bobWishList = {'Atomic Habits', 'Clean Code', 'The Pragmatic Programmer'}
+    print('Welcome to the Bookstore Inventory System')
+    print('Current Inventory:', inventory)
+    print('Harry\'s Wishlist:', harryWishList)
+    print('Bob\'s Wishlist:', bobWishList)
+    option = int(input('Choose an option: \n1. Add Inventory\n2. Remove Inventory\n3. Exit\n'))
 
-    inventory = addInventory(inventory)
-    print(inventory)
+    match option:
+        case 1:
+            inventory = addInventory(inventory)
+            print('Updated Inventory:', inventory)
+        case 2:
+            inventory = removeInventory(inventory)
+            print('Updated Inventory:', inventory)
+        case _:
+            print('Enter A Valid Option')
 
+
+    
 main()
